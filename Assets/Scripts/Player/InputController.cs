d1e1dbdbfc97c757ed37d7b48152713f046aa342
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2021-04-30 13:28:12 +0200

    Created a simple character controller.

---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InputController: MonoBehaviour
+{
+    private SimpleCharacterController charController;
+
+    void Awake()
+    {
+        charController = GetComponent<SimpleCharacterController>();
+    }
+
+    private void Update()
+    {
+        // Get input values
+        int vertical = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+        int horizontal = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
+        bool jump = Input.GetKey(KeyCode.Space);
+
+        charController.ForwardInput = vertical;
+        charController.TurnInput = horizontal;
+        charController.JumpInput = jump;
+    }
+}
