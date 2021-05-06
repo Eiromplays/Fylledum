25fc30153d3971e8c3b372dc966716e8dc36674a
joaisa <73830719+joaisa@users.noreply.github.com>
2021-05-06 15:04:55 +0200

    Update PlayerController.cs
    Added sprint with stamina, and changed the movement behavior while in air

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,13 @@ namespace Assets.Scripts.Player
     public class PlayerController : MonoBehaviour
     {
         public float speed = 7.5f;
+        public float maxSpeed = 50f;
+
+        public float sprintMultiplier = 3f;
+        public float sprintLength = 10f;
+        public float staminaMultiplier = 20f;
+        public float staminaRegainCooldown = 1f;
+
         public float jumpSpeed = 8.0f;
         public float gravity = 20.0f;
         public Transform playerCameraParent;
@@ -17,6 +24,10 @@ namespace Assets.Scripts.Player
         Vector3 _moveDirection = Vector3.zero;
         Vector2 _rotation = Vector2.zero;
 
+        float stamina = 100f;
+        float defaultSpeed = 0;
+        float cooldownTimer = 0;
+
         [HideInInspector]
         public bool canMove = true;
 
@@ -25,20 +36,50 @@ namespace Assets.Scripts.Player
             Cursor.lockState = CursorLockMode.Locked;
             _characterController = GetComponent<CharacterController>();
             _rotation.y = transform.eulerAngles.y;
+
+            defaultSpeed = speed;
         }
 
         // ReSharper restore Unity.ExpensiveCode
         // ReSharper disable Unity.PerformanceAnalysis
         private void Update()
         {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+
+            float currentY = _moveDirection.y;
+            _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            _moveDirection.y = currentY;
+
+            _moveDirection.x = Mathf.Clamp(_moveDirection.x, -maxSpeed, maxSpeed);
+            _moveDirection.z = Mathf.Clamp(_moveDirection.z, -maxSpeed, maxSpeed);
+
+            // Sprinting calculations
+
+            if(Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+            {
+                speed = defaultSpeed * sprintMultiplier;
+                cooldownTimer = 0f;
+
+                stamina -= 1f * Time.deltaTime * staminaMultiplier;
+            }
+
+            else
+            {
+                speed = defaultSpeed;
+
+                cooldownTimer = Mathf.Clamp(cooldownTimer + (1f * Time.deltaTime), 0f, staminaRegainCooldown);
+
+                stamina += (cooldownTimer == staminaRegainCooldown) ? 1f * Time.deltaTime * staminaMultiplier : 0f;
+                stamina = Mathf.Clamp(stamina, 0f, 100f);
+            }
+
+
             if (_characterController.isGrounded)
             {
-                // We are grounded, so recalculate move direction based on axes
-                Vector3 forward = transform.TransformDirection(Vector3.forward);
-                Vector3 right = transform.TransformDirection(Vector3.right);
-                float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
-                float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
-                _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+                // We are grounded, so check for jump input
 
                 if (Input.GetButton("Jump") && canMove)
                 {
